6b7471dec3aabb962557d4086e0a5ce2548b1ca1
Shaoyu <shaoyu@life-foundry.com>
2020-09-26 16:46:51 -0500

    add elf tutorial

---
+++ javascript/tutorial_elf.js
@@ -0,0 +1,21 @@
+fetch("binary/hello").then(response => response.arrayBuffer()).then((buffer) => {
+  // var buffer = new Uint8Array([0x7F, 0x45, 0x4C, 0x46, ...]);
+  var elf = new Elf(buffer);
+  if (elf.kind() != "elf") {
+    throw "Not an ELF file";
+  }
+
+  var ehdr = elf.getehdr();
+  document.getElementById("ehdr").innerHTML = JSON.stringify(ehdr);
+
+  // Handle segments
+  for (var i = 0; i < ehdr.phnum; i++) {
+    var phdr = elf.getphdr(i);
+  }
+  // Handle sections
+  for (var i = 0; i < ehdr.shnum; i++) {
+    var scn = elf.getscn(i);
+    var shdr = elf.getshdr(scn);
+    var name = elf.strptr(ehdr.e_shstrndx.num(), shdr.sh_name.num());
+  }
+});
