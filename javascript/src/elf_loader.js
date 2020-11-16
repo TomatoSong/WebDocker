8a2e2196541886e0b8c7ee1fecfb3052ee8629a6
smeng9 <38666763+smeng9@users.noreply.github.com>
2020-11-16 17:37:25 -0600

    Update elf_loader.js

--- javascript/src/elf_loader.js
+++ javascript/src/elf_loader.js
@@ -19,7 +19,7 @@ function set_up_stack(command)
 
 	// Program name
 	stack_pointer -= command[0].length;
-	unicorn.mem_write(stack_pointer, new ElfUInt64(command[0]).chunks);
+	unicorn.mem_write(stack_pointer, new ArrayBuffer(command[0]));
 
 	// Environment string
 	// Empty for now
@@ -29,7 +29,7 @@ function set_up_stack(command)
 	{
 		stack_pointer -= 1; // NULL termination of string
 		stack_pointer -= command[i].length;
-		unicorn.mem_write(stack_pointer, new ElfUInt64(command[i]).chunks);
+		unicorn.mem_write(stack_pointer, new ArrayBuffer(command[i]));
 
 		argv_pointers.push(stack_pointer);
 	}
@@ -53,8 +53,8 @@ function set_up_stack(command)
 		unicorn.mem_write(stack_pointer, new ElfUInt64(argv_pointers[i]).chunks);
 	}
 
-	// Argc
-	stack_pointer -= 4;
+	// Argc (which is 64 bit)
+	stack_pointer -= 8;
 	unicorn.mem_write(stack_pointer, new ElfUInt64(command.length).chunks);
 
 	// Set stack pointer
