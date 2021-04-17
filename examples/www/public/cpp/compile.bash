a10ac2168e6ec99bf9c40374d15eb6434d56cf3a
root <root>
2021-04-17 15:29:31 -0500

    move things around

--- examples/www/public/cpp/compile.bash
+++ examples/www/public/cpp/compile.bash
@@ -2,6 +2,6 @@ for src in $(ls); do
 	if [[ "$src" == *".c"* ]] || [[ "$src" == *".cpp"* ]]; then
 		bin=${src%".c"}
 		bin=${bin%".cpp"}
-		musl-gcc -static $src -o ../bin/$bin
+		musl-gcc -static $src -o bin/$bin
 	fi
 done
