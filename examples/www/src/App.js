f91d683c50d24b73052949ed8eb684f0a1a7b8ca
root <root>
2021-04-18 17:59:57 -0500

    make docker registry static

--- examples/www/src/App.js
+++ examples/www/src/App.js
@@ -1,26 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
-import Card from "react-webdocker"
+import WebDocker from "react-webdocker"
+
+import { Nav } from "./Nav.js"
 
 function App() {
   return (
     <div className="App">
-      <Card />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <WebDocker />
     </div>
   );
 }
