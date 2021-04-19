0e81889aa7961d19f7b38d328e03223f55d042af
root <root>
2021-04-18 23:06:28 -0500

    add more description

--- examples/www/src/App.js
+++ examples/www/src/App.js
@@ -4,12 +4,16 @@ import './App.css';
 import WebDocker from "react-webdocker"
 
 import { Nav } from "./Nav.js"
+import { Hero } from "./Hero.js"
 
 function App() {
   return (
     <div className="App">
       <Nav />
-      <WebDocker />
+      <main>
+        <Hero />
+        <WebDocker />
+      </main>
     </div>
   );
 }
